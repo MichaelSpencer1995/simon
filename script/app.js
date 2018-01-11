1caa5d9104a3232ac86eb17f7ff88f088b215025
michael.spencer1995 <michael.spencer1@g.austincc.edu>
2018-01-11 07:11:27 -0800

    first prototype

---
+++ script/app.js
@@ -0,0 +1,295 @@
+var moves = []//array of current moves
+var counter = 0;
+var count = 1;
+var turnedOff = true;
+var showingCorrectMove = false;
+var playerCount;
+var isPlaying = false;
+var isStrict = false;
+var findingNextToShow = false;
+var failedNotStrictMode = false;
+var moveToCopy;
+var singingNote;
+
+// cancel set time outs
+var c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17;
+
+var red = document.getElementById('red');
+var yellow = document.getElementById('yellow');
+var green = document.getElementById('green');
+var blue = document.getElementById('blue');
+
+var start = document.getElementById('start');
+var onOff = document.getElementById('on-off');
+var strict = document.getElementById('strict');
+var countDisplay = document.getElementById('counter');
+
+// functionality buttons
+start.addEventListener('click', handleStart);
+onOff.addEventListener('click', handleOnOff);
+strict.addEventListener('click', handleStrict);
+// main color buttons
+red.addEventListener('click', handleColor.bind(red));
+yellow.addEventListener('click', handleColor.bind(yellow));
+green.addEventListener('click', handleColor.bind(green));
+blue.addEventListener('click', handleColor.bind(blue));
+
+// functionality buttons
+function handleOnOff() {
+    if(turnedOff) {
+        onOff.classList.add('onOff-active');
+        countDisplay.textContent = '--';
+
+    } else {
+        onOff.classList.remove('onOff-active');
+        start.classList.remove('start-active');
+        strict.classList.remove('strict-active');
+        
+        lockAll();
+        clearTimeout(c1);
+        clearTimeout(c2);
+        clearTimeout(c3);
+        clearTimeout(c4);
+        clearTimeout(c5);
+        clearTimeout(c6);
+        clearTimeout(c7);
+        clearTimeout(c8);
+        clearTimeout(c9);
+        clearTimeout(c10);
+        clearTimeout(c11);
+        clearTimeout(c12);
+        clearTimeout(c13);
+        clearTimeout(c14);
+        clearTimeout(c15);
+        clearTimeout(c16);
+        clearTimeout(c17);
+        
+
+        isPlaying = false;
+        isStrict = false;
+        countDisplay.textContent = '';
+        moves = [];
+
+
+    }
+    turnedOff = !turnedOff;
+
+}
+
+function handleStrict() {
+    if(turnedOff || isPlaying) {
+        return;
+
+    }
+
+    if(isStrict) {
+        isStrict = false;
+        strict.classList.remove('strict-active');
+        
+    } else {
+        isStrict = true;
+        strict.classList.add('strict-active');
+        
+    }
+
+}
+
+
+
+function handleStart() {
+    if(turnedOff || isPlaying) {
+        return;
+
+    }
+
+    isPlaying = true;
+    start.classList.add('start-active');
+    startGame();
+
+}
+
+//start game
+function startGame() {
+    reset();
+
+    c1 = setTimeout(function() {
+        addMove();
+    
+    }, 1000);
+
+}
+
+
+function addMove(currentMoves) {
+    lockAll();
+    var nextMove = ranNumBetween(0,3);
+
+    moves.push(nextMove);
+    //add next move to moves array
+
+    c2 = setTimeout(function() {
+        findNextToShow();
+
+    }, 1000);
+
+
+}
+
+function findNextToShow() {
+  // show the pattern to match
+    findingNextToShow = true;
+
+    var nextMove = moves[counter];
+
+    if(isStrict && showingCorrectMove) {
+        nextMove = moves[moveToCopy];
+
+    }
+
+    if(nextMove === 0) {
+        playNext.call(red);
+
+    } else if(nextMove === 1) {
+        playNext.call(yellow);
+
+    } else if(nextMove === 2) {
+        playNext.call(green);
+
+    } else if(nextMove === 3) {
+        playNext.call(blue);
+
+    } else {
+        countDisplay.textContent = count++;
+        playersTurn();
+        return;
+
+    }
+    counter++;
+
+
+}
+
+
+function playNext() {
+    var self = this;
+
+    self.style.backgroundColor = self.button.color2;
+    self.button.sound.play();
+
+    c3 = setTimeout(function() {
+        self.style.backgroundColor = self.button.color1;
+
+        c4 = setTimeout(function() {
+            if(showingCorrectMove) {
+                if(isStrict) {
+                    isPlaying = false;
+                    handleStart();
+                    return;
+
+                }
+
+            }
+
+            findNextToShow();
+
+        }, 700);
+
+    }, 300);
+
+}
+
+
+function playersTurn() {
+    showingCorrectMove = false;
+    findingNextToShow = false;
+    playerCount = 0
+    moveToCopy = 0;
+    unlockAll();
+
+}
+
+
+function showCorrectMove() {
+    showingCorrectMove = true;
+    // isPlaying = false;
+
+    c5 = setTimeout(function() {
+        countDisplay.textContent = 'XX';
+
+        c6 = setTimeout(function() {
+            countDisplay.textContent = '--';
+
+            c7 = setTimeout(function() {
+                countDisplay.textContent = 'XX';
+
+                c8 = setTimeout(function() {
+                    countDisplay.textContent = '--';
+
+                    c9 = setTimeout(function() {
+                        countDisplay.textContent = 'XX';
+                        findNextToShow();
+
+                    }, 400);
+
+                }, 400);
+
+            }, 400);
+
+        }, 400);
+
+    }, 400);
+
+}
+
+
+function handleColor() {
+    if(showingCorrectMove || findingNextToShow) {
+        console.log('returned', showingCorrectMove, findingNextToShow)
+        return;
+
+    }
+
+    console.log('color pressed')
+    
+    var self = this;
+    var lastMove = moveToCopy + 1 === moves.length;
+    var correctMove = self.button.index === moves[moveToCopy];
+    
+    self.style.backgroundColor = self.button.color2;
+    self.button.sound.play();
+
+    if(correctMove) {
+        if(lastMove) {
+                
+            if(counter === 3) {
+                countDisplay.textContent = 'YOU WON!';
+                userWon();
+                return;
+
+            }
+
+            findingNextToShow = true;
+            counter = 0;
+            addMove();
+
+        } else {
+            moveToCopy++;
+
+        }
+
+    } else {
+        counter = 0;
+        count--;
+        lockAll();
+        showCorrectMove();
+
+
+    }
+
+    setTimeout(function() {
+        self.style.backgroundColor = self.button.color1;
+
+    }, 300);
+
+}
+
---
+++ script/app2.js
@@ -0,0 +1,108 @@
+function SimonButton (index, sound, color1, color2, frequency) {
+    this.index = index;
+    this.sound = new Audio(sound);
+    this.color1 = color1;
+    this.color2 = color2;
+    this.frequency = frequency;
+
+}
+
+red.button = new SimonButton(0, './sounds/simonSound1.mp3', 'red', 'darkred', 329.63);
+yellow.button = new SimonButton(1, './sounds/simonSound2.mp3', 'yellow', 'gold', 261.63);
+green.button = new SimonButton(2, './sounds/simonSound3.mp3', 'green', 'darkgreen', 220);
+blue.button = new SimonButton(3, './sounds/simonSound4.mp3', 'blue', 'darkblue', 164.81);
+
+function ranNumBetween(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+
+}
+
+function lockAll() {
+    console.log('locked')
+    red.setAttribute('disabled', 'true');
+    green.setAttribute('disabled', 'true');
+    yellow.setAttribute('disabled', 'true');
+    blue.setAttribute('disabled', 'true');
+
+}
+
+function unlockAll() {
+    console.log('unlocked')
+    red.removeAttribute("disabled");
+    green.removeAttribute("disabled");
+    yellow.removeAttribute("disabled");
+    blue.removeAttribute("disabled");
+
+}
+
+function reset() {
+    moves = [];
+    countDisplay.textContent = '--';
+    counter = 0;
+    count = 1; // so count display starts at 0 and not 1
+    showingCorrectMove = false;
+    playerCount = 0;
+    findingNextToShow = false;
+    
+}
+
+
+function userWon() {
+    lockAll();
+
+    c10 = setTimeout(function() {
+        strobeOne();
+
+        c11 = setTimeout(function() {
+            strobeTwo();
+
+            c12 = setTimeout(function() {
+                strobeOne();
+
+                c13 = setTimeout(function() {
+                    strobeTwo();
+                    
+                    c14 = setTimeout(function() {
+                        strobeOne();
+                
+                        c15 = setTimeout(function() {
+                            strobeTwo();
+
+                            c16 = setTimeout(function() {
+                                strobeOne();
+
+                                c17 = setTimeout(function() {
+                                    strobeTwo();
+                                    strict.classList.add('strict-active');
+                                    
+                                }, 400);
+        
+                            }, 400);
+
+                        }, 400);
+
+                    }, 400);
+
+                }, 400);
+
+            }, 400);
+
+        }, 400);
+
+    }, 400);
+    
+}
+
+function strobeOne() {
+    onOff.classList.remove('onOff-active');
+    start.classList.remove('start-active');
+    strict.classList.add('strict-active');
+
+}
+
+function strobeTwo() {
+    onOff.classList.add('onOff-active');
+    start.classList.add('start-active');
+    strict.classList.remove('strict-active');
+
+}
